4bc932301ecbae8777d98b2a44f5c4ae99d0cf5a
Hygor Braga <hygortbraga@hotmail.com>
2022-04-01 18:03:40 -0300

    primeiro commit

---
+++ Aula1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ganharei 30k por mês");
+        }
+    }
+}
